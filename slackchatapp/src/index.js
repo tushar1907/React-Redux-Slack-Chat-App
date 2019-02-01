5c4f55f71af1bf13587128125d469a4702471dbf
Tushar <gupta.tus@husky.neu.edu>
2019-02-01 08:46:00 -0500

    Refister form completed

--- slackchatapp/src/index.js
+++ slackchatapp/src/index.js
@@ -8,6 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';   
 
+import 'semantic-ui-css/semantic.min.css';
 
 const Root = () =>(
     <Router>    
