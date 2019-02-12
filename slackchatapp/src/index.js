2c3ab1aa05081ba03a73451f90ca5eb779a48e85
Tushar <gupta.tus@husky.neu.edu>
2019-02-12 05:34:01 -0500

    Login Route setup withRouter

--- slackchatapp/src/index.js
+++ slackchatapp/src/index.js
@@ -5,23 +5,39 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
+import firebase from './FireBase'
 
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';   
+import { BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';   
 
 import 'semantic-ui-css/semantic.min.css';
 
-const Root = () =>(
-    <Router>    
-        <Switch>
-            <Route exact path='/' component={App}/>
-            <Route path='/login' component={Login}/>
-            <Route path='/register' component={Register}/>
-        </Switch>
-    </Router>
-);
-    
+class Root extends React.Component{
 
-ReactDOM.render(<Root/>, document.getElementById('root'));
+    componentWillUnmount = () => {
+      firebase.auth().onAuthStateChanged(user=>{
+          if(user){
+              this.props.history.push("/");
+          }
+      })
+    };
+    
+    render(){
+      return  (
+            
+                <Switch>
+                    <Route exact path='/' component={App}/>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/register' component={Register}/>
+                </Switch>
+            
+        ) 
+    }
+}
+    
+const RootWithAuth = withRouter(Root);
+
+
+ReactDOM.render(<Router><RootWithAuth/></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
