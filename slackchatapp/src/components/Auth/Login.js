74bbb32cecc624e10a5ec9c28614ed96adef3bb7
Tushar <gupta.tus@husky.neu.edu>
2019-02-12 04:49:23 -0500

    login form created with validation

--- slackchatapp/src/components/Auth/Login.js
+++ slackchatapp/src/components/Auth/Login.js
@@ -1,10 +1,105 @@
 import React from 'react';
 
-class Login extends React.Component{
+import { Grid, From, Segment, Button, Header, Message, Icon, Form } from 'semantic-ui-react';
 
+import { Link } from 'react-router-dom';
+
+import firebase from '../../FireBase'
+
+
+
+class Login extends React.Component{
+    state = {
+        
+        email: '',
+        password: '',
+       
+        errors: [],
+        loading: false,
+        
+    }
+
+    
+
+    
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>)
+
+    handleChange = event =>{
+        this.setState({ [event.target.name]: event.target.value })        
+    }
+
+    handleSubmit = event =>{
+        if(this.isFormValid(this.state)){
+            this.setState({errors:[], loading: true}) 
+            firebase.auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+                .then(signedInUser=>{
+                    console.log(signedInUser)
+                    console.log('User Signed In')
+                })
+                .catch(err=>{
+                    console.log()
+                    this.setState({
+                        errors: this.state.errors.concat(err),
+                        loading: false
+                    })
+                })    
+            
+        }        
+    }
+
+    isFormValid = ({email,password})=> email && password
+
+  
+
+
+    handleInputError(errors, inputName){
+
+       return errors.some(error => 
+        error.message.toLowerCase().includes(inputName)
+        ) 
+        ? 'error' 
+        :''
+
+    }
     render(){
+        const { email, password, errors, loading } = this.state;
         return(
-            <div>Login</div>
+            <Grid textAlign='center' verticalAlign='middle' className="app">
+                <Grid.Column style={{ maxWidth:500 }}>
+                    <Header as='h1' icon color='violet' textAlign='center'>
+                        <Icon name='code branch' color='violet' />
+                        LOGIN TO DEV CHAT
+                    </Header>
+                    <Form onSubmit={this.handleSubmit} size='large'>
+                        <Segment stacked>
+
+                            
+                            <Form.Input fluid name='email' icon='mail' iconPosition='left' placeholder='Email' 
+                            onChange={this.handleChange} type='email' value={email}
+                            className={this.handleInputError(errors, 'email')}/>  
+
+                            <Form.Input fluid name='password' icon='lock' iconPosition='left' placeholder='Password' 
+                            onChange={this.handleChange} type='password' value={password}
+                            className={this.handleInputError(errors, 'password')}/>  
+
+                            
+
+                            <Button disabled={loading} className={loading ? 'loading':''} color='violet' fluid size='large'>Submit</Button>
+                        </Segment>
+                    </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                        <h3>
+                           Error
+                        </h3>
+                        {this.displayErrors(errors)}
+                        </Message>
+                    )}
+                    <Message>Don't have an account ?<Link to='/register'> Register here </Link></Message>
+                </Grid.Column>
+            </Grid>
         )
     }
 }
--- slackchatapp/src/components/Auth/Register.js
+++ slackchatapp/src/components/Auth/Register.js
@@ -107,7 +107,7 @@ class Register extends React.Component{
         return(
             <Grid textAlign='center' verticalAlign='middle' className="app">
                 <Grid.Column style={{ maxWidth:500 }}>
-                    <Header as='h2' icon color='blue' textAlign='center'>
+                    <Header as='h1' icon color='blue' textAlign='center'>
                         <Icon name='tasks' color='blue' />
                         USER REGISTRATION
                     </Header>
