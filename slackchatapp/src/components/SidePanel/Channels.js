a1b573511955b2545a3c5d602b8e70167db31da5
Tushar <gupta.tus@husky.neu.edu>
2019-02-21 02:01:30 -0500

    Create Channels Collection, Add First Channel to Database

--- slackchatapp/src/components/SidePanel/Channels.js
+++ slackchatapp/src/components/SidePanel/Channels.js
@@ -1,22 +1,72 @@
 import React, { Component } from 'react'
 import { Menu, MenuItem, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import firebase from '../../FireBase';
 
 export default class Channels extends Component {
 
     state = {
+        user: this.props.currentUser,
         channels: [],
         modal: false,
         channelName: '',
-        channelDetails: ''
+        channelDetails: '',
+        channelRef: firebase.database().ref('channels')
     }
+    
 
     closeModal=()=> this.setState({modal: false});
 
     openModal=()=> this.setState({modal: true});
 
-    handleChange = (event) =>{
+    handleChange = (event) =>{      
       this.setState({[event.target.name]: event.target.value})
     }
+
+    handleSubmit = event =>{
+      event.preventDefault();     
+      if(this.isFormValid(this.state)){
+        
+        this.addChannel();
+      }
+    }
+
+    addChannel = () =>{
+      const {channelRef, channelName, channelDetails, user} = this.state;
+
+      const key = channelRef.push().key;
+     
+      const newChannel = {
+        id: key,
+        name: channelName,
+        details: channelDetails,
+        createdBy:{
+          name: user.displayName,
+          avatar: user.photoURL
+        }
+      };
+
+        channelRef  
+          .child(key)
+          .update(newChannel)
+          .then(()=>{
+            this.setState({channelDetails:'', channelName:''})
+            this.closeModal();
+            console.log('channel added')
+          })
+          .catch(err=>{
+            console.error(err)
+          })
+
+
+      } 
+
+
+    
+
+
+    isFormValid = ({channelName, channelDetails}) => channelName && channelDetails;
+
+
   render() {
     const {channels,modal} = this.state;
     return (
@@ -39,21 +89,21 @@ export default class Channels extends Component {
 
             <Modal.Header>All a Channel</Modal.Header>
             <Modal.Content>
-              <Form>
+              <Form onSubmit={this.handleSubmit}>
 
                 <Form.Field>
                   <Input fluid label="Name of Channel" name='channelName' onChange={this.handleChange}/>
                 </Form.Field>
 
                 <Form.Field>
-                  <Input fluid label="About the Channel" name='channelDetail' onChange={this.handleChange}/>
+                  <Input fluid label="About the Channel" name='channelDetails' onChange={this.handleChange}/>
                 </Form.Field>
 
               </Form>
             </Modal.Content>
 
             <Modal.Actions>
-              <Button color='green' onClick={this.closeModal} inverted>
+              <Button color='green' onClick={this.handleSubmit} inverted>
               <Icon name='checkmark' /> Add
               </Button>
 
