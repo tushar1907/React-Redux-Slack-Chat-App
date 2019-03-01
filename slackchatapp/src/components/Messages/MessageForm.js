e5aadd13b73c867b69933466f9204dc228c5158e
Tushar <gupta.tus@husky.neu.edu>
2019-03-01 03:51:28 -0500

    Scaffold ColorPanel / Message Components done

---
+++ slackchatapp/src/components/Messages/MessageForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Segment, Button, Input} from 'semantic-ui-react';
+
+class MessageForm extends React.Component{
+    render(){
+        return(
+            <Segment className='message__form'>
+                <Input 
+                fluid
+                name='message'
+                style={{ marginBottom: '0.7em'}}
+                label={<Button icon='add'/>}
+                labelPosition='left'
+                placeholder="Write your message"
+                />
+                <Button.Group icon widths='3'>
+                    <Button color='dark green' border content='Add Reply' labelPosition='left' icon='edit'/>
+                    <Button color='teal' border content='Upload Media' labelPosition='right' icon='cloud upload'/>
+                </Button.Group>
+            </Segment>
+        )
+    }
+}
+
+export default MessageForm;
--- slackchatapp/src/components/Messages/Messages.js
+++ slackchatapp/src/components/Messages/Messages.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Segment, Comment, MessageHeader} from 'semantic-ui-react';
+import MessageForm from './MessageForm';
+import MessagesHeader from './MessagesHeader';
 
 class Messages extends React.Component{
     render(){
         return(
-            <div>
-                Messages
-            </div>
+            <React.Fragment>
+                <MessagesHeader/>
+                <Segment>
+                    <Comment.Group className='messages'>
+                    
+                    </Comment.Group>
+                </Segment>
+
+                <MessageForm/>
+            </React.Fragment>
         )
     }
 }
