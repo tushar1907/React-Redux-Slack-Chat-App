64e3145487080bd9c1cfee684422f09ce5f186b2
Tushar <gupta.tus@husky.neu.edu>
2019-03-03 21:20:19 -0500

    Put Media File in State, Add Mimetype Validation done

--- slackchatapp/src/components/Messages/FileModal.js
+++ slackchatapp/src/components/Messages/FileModal.js
@@ -1,54 +1,65 @@
-import React from 'react';
-import { Segment, Button, Input,Modal,Icon} from 'semantic-ui-react';
-import firebase from '../../FireBase';
-
-
+import React from "react";
+import mime from "mime-types";
+import { Modal, Input, Button, Icon } from "semantic-ui-react";
 
 class FileModal extends React.Component {
-    state = {
-      message: "",
-      channel: this.props.currentChannel,
-      user: this.props.currentUser,
-      loading: false,
-      errors: [],
-      modal:false
-    };
-  
+  state = {
+    file: null,
+    authorized: ["image/jpeg", "image/png"]
+  };
 
-    render() {
-      const { modal, closeModal } = this.props;
-  
-      return (
-        <Modal basic open={modal} onCLose={closeModal}>
-            <Modal.Header>Select an Image File</Modal.Header>
-            <Modal.Content>
-                <Input
-                fluid
-                label="File types: jpg, png, pdf, docx"
-                name="file"
-                type="file"
-                />
-            </Modal.Content>
-
-            <Modal.Actions>
-                <Button
-                color="green"
-                inverted>
-                <Icon name="checkmark"/> Send
-                </Button>
-
-                <Button
-                color="red"
-                inverted
-                onClick={closeModal}
-                >
-                <Icon name="remove"/> Cancel
-                </Button>
-            </Modal.Actions>
-        </Modal>
-      );
+  addFile = event => {
+    const file = event.target.files[0];
+    if (file) {
+      this.setState({ file });
     }
+  };
+
+  sendFile = () => {
+    const { file } = this.state;
+    const { uploadFile, closeModal } = this.props;
+
+    if (file !== null) {
+      if (this.isAuthorized(file.name)) {
+        const metadata = { contentType: mime.lookup(file.name) };
+        uploadFile(file, metadata);
+        closeModal();
+        this.clearFile();
+      }
+    }
+  };
+
+  isAuthorized = filename =>
+    this.state.authorized.includes(mime.lookup(filename));
+
+  clearFile = () => this.setState({ file: null });
+
+  render() {
+    const { modal, closeModal } = this.props;
+
+    return (
+      <Modal basic open={modal} onClose={closeModal}>
+        <Modal.Header>Select an Image File</Modal.Header>
+        <Modal.Content>
+          <Input
+            onChange={this.addFile}
+            fluid
+            label="File types: jpg, png"
+            name="file"
+            type="file"
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={this.sendFile} color="green" inverted>
+            <Icon name="checkmark" /> Send
+          </Button>
+          <Button color="red" inverted onClick={closeModal}>
+            <Icon name="remove" /> Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    );
   }
-  
-  export default FileModal;
-  
+}
+
+export default FileModal;
--- slackchatapp/src/components/Messages/Message.js
+++ slackchatapp/src/components/Messages/Message.js
@@ -20,3 +20,4 @@ const Message = ({ message, user }) => (
 );
 
 export default Message;
+ 
